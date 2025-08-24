ba6303cc401ab75ec04cb588ac6303107e3f8d0b
Junaid Mokit <junaid.rahman.mokit096@gmail.com>
2025-08-24 17:05:49 +0600

    use loop

--- Program.cs
+++ Program.cs
@@ -6,41 +6,49 @@ class Test
     public static void Main(string[] args)
     {
 
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
-        int sum = num1 + num2;
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        // int sum = num1 + num2;
 
-        Console.WriteLine("Sum=" + sum);
+        // Console.WriteLine("Sum=" + sum);
 
-        if (sum > 5)
+        // if (sum > 5)
+        // {
+        //     Console.WriteLine("This is a good number");
+
+        // }
+        // else if (sum > 7)
+        // {
+        //     Console.WriteLine("This is a good number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This is bad number");
+        // }
+
+        // switch (sum)
+        // {
+        //     case int n when n > 7:
+        //         Console.WriteLine("This is a good number");
+        //         break;
+
+        //     case int n when n > 5:
+        //         Console.WriteLine("This is a moderate number");
+        //         break;
+
+        //     default:
+        //         Console.WriteLine("This is Bad Number");
+        //         break;
+
+
+        // }
+
+
+        int[] numbers = { 10, 30, 40, 60 };
+
+        for (int i = 0; i < numbers.Length; i++)
         {
-            Console.WriteLine("This is a good number");
-
-        }
-        else if (sum > 7)
-        {
-            Console.WriteLine("This is a good number");
-        }
-        else
-        {
-            Console.WriteLine("This is bad number");
-        }
-
-        switch (sum)
-        {
-            case int n when n > 7:
-                Console.WriteLine("This is a good number");
-                break;
-
-            case int n when n > 5:
-                Console.WriteLine("This is a moderate number");
-                break;
-
-            default:
-                Console.WriteLine("This is Bad Number");
-                break;
-
-
+            Console.WriteLine($"The {i} number is {numbers[i]}");
         }
 
     }
